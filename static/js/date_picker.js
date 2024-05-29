6cd987c00766751d8ae25c87e8683c9256e4ea53
Dennis Schenkel <dennisschenkel@googlemail.com>
2024-05-29 22:10:07 +0200

    Fix correct date in date picker

--- static/js/date_picker.js
+++ static/js/date_picker.js
@@ -1,16 +1,43 @@
 
 // Get the current date and set it as the default value for the date picker
 
-// Define the current date
-var currentDate = new Date();
-var day = currentDate.getDate();
-var month = currentDate.getMonth() + 1;
-var year = currentDate.getFullYear();
-var formattedDate = year + '-' + month + '-' + day;
+// Get the current date in the format yyyy-mm-dd
+function getCurrentDate() {
+    const currentDate = new Date();
+    const day = currentDate.getDate();
+    const month = addjustMonth(currentDate)
+    const year = currentDate.getFullYear();
+    const currentFormattedDate = year + '-' + month + '-' + day;
+    return currentFormattedDate;
+}
+
+// Add a zero to the month if it is less than 10
+function addjustMonth(currentDate){
+    if (currentDate.getMonth() < 9) { 
+        return '0' + (currentDate.getMonth() + 1); 
+        } 
+        else { 
+            return currentDate.getMonth() + 1; 
+        };
+}
+
+// Get the date from the URL
+function getDateFromURL() {
+    const urlDate = new URLSearchParams(window.location.search);
+    return urlDate.get('date');
+}
+
+// Get the final formatted date for datepicker
+function getFormattedDate() {
+    const dateFromURL = getDateFromURL();
+    console.log(getCurrentDate());
+    console.log(dateFromURL);
+    return dateFromURL ? dateFromURL : getCurrentDate();
+}
 
 // Set the default value of the date picker to the current date
 $(document).ready(function() {
-    $('#datepicker').val(formattedDate);
+    $('#datepicker').val(getFormattedDate());
 });
 
 // Add calendar functionality to the date picker
@@ -18,7 +45,7 @@ $('.datepicker').datepicker({
     format: 'yyyy-mm-dd',
     autoclose: true
 }).on('changeDate', function() {
-    var selectedDate = $(this).val();
+    const selectedDate = $(this).val();
     window.location.href = '/?date=' + selectedDate;
 });
 
--- static/js/script.js
+++ static/js/script.js
@@ -1,20 +1,36 @@
 
-// Toggle for mobile sidebar
 
+
+// Toggle open mobile sidebar
 $(document).ready(function() {
-    // Toggle sidebar visibility using a class
     $('#sidebarToggle').click(function() {
-        $('#sidebar-menu').toggleClass('d-none');
+        $('#sidebar-menu').removeClass('d-none');
     });
 });
 
+// Toggle close mobile sidebar
+$(document).ready(function() {
+    $('#closeSidebar').click(function() {
+        $('#sidebar-menu').addClass('d-none');
+    });
+});
+
+// Close mobile sidebar when clicking outside
+/*
+$(document).ready(function() {
+    $(document).click(function() {
+        if (!$('#sidebar-menu').hasClass('d-none')) {
+            $('#sidebar-menu').addClass('d-none');
+        }
+    });
+});
+*/
 
 // Toggle for dropdown menu in profile
-
 $(document).ready(function() {
     $(".profile").click(function(event) {
         event.stopPropagation();
-        var $dropdown = $(this).find(".profil-options-dropout");
+        const $dropdown = $(this).find(".profil-options-dropout");
         $(".profil-options-dropout").not($dropdown).addClass("visually-hidden");
         $dropdown.toggleClass("visually-hidden");
     });
