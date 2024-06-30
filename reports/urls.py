45f1355bed90fd5f7593c6ee77e76d7ac4756842
Dennis Schenkel <dennisschenkel@googlemail.com>
2024-06-30 19:35:34 +0200

    Add edit report feature

--- reports/urls.py
+++ reports/urls.py
@@ -1,9 +1,11 @@
 from .views.reports_list_views import reports_list_view
 from .views.add_report_views import add_report_view
+from .views.add_report_views import update_report_view
 from django.urls import path
 
 urlpatterns = [
     path('', reports_list_view, name='home'),
     path('reports/', reports_list_view, name='reports_list_temp'),
     path('reports/add_report/', add_report_view, name='add_report_temp'),
+    path('reports/update_report/<int:id>', update_report_view, name='update_report_temp'),
 ]
--- reports/views/add_report_views.py
+++ reports/views/add_report_views.py
@@ -28,9 +28,40 @@ def add_report_view(request):
                 messages.SUCCESS,
                 f"Report for { report_date } has been added successfully.",
             )
-            return HttpResponseRedirect("/")
+            return HttpResponseRedirect(f"/reports/?date={report_date}")
 
     else:
         form = AddReport()
 
     return render(request, "reports/add_report_temp.html", {"form": form})
+
+
+# Function for updating report
+def update_report_view(request, id):
+    """_summary_
+
+    Args:
+        request (_type_): _description_
+        id (_type_): _description_
+
+    Returns:
+        _type_: _description_
+    """
+
+    report = get_object_or_404(Report, id=id)
+    report_date = report.report_date
+    if request.method == "POST":
+        form = AddReport(request.POST, request.FILES, instance=report)
+        if form.is_valid():
+            form.save()
+            messages.add_message(
+                request,
+                messages.SUCCESS,
+                f"Report for {report_date} has been updated successfully.",
+            )
+            return HttpResponseRedirect(f"/reports/?date={report_date}")
+
+    else:
+        form = AddReport(instance=report)
+
+    return render(request, "reports/update_report_temp.html", {"form": form, "report": report})
