cd05f9a6b517e4d33545540adf865194b231f4e7
Dennis Schenkel <dennisschenkel@googlemail.com>
2024-07-05 11:12:44 +0200

    Fix issue with modal not closing on close button click

--- static/js/guest_list.js
+++ static/js/guest_list.js
@@ -3,26 +3,26 @@ $(document).ready(function() {
     // Toggle for dropout menu in profile
     $(".profile").click(function(event) {
         // Check if the click is on a modal-trigger element, so that it doesn't stop the opening of the modal
-        if (!$(event.target).hasClass('modal-trigger')) {
+       if (!$(event.target).hasClass('modal-trigger')) {
             event.stopPropagation();
             const $dropout = $(this).find(".profile-options-dropout");
             const $guestImg = $(this).find(".guest-img");
             const $alerts = $(this).find(".alerts");
-            const $alertsDrowout = $(this).find(".alerts-dropout");
+            const $alertsDropout = $(this).find(".alerts-dropout");
             $(".profile-options-dropout").not($dropout).addClass("visually-hidden");
             $(".guest-img").not($guestImg).removeClass("img-prev-md").addClass("img-prev-sm");
             $dropout.toggleClass("visually-hidden");
             $guestImg.toggleClass("img-prev-md img-prev-sm");
             $(".alerts").not($alerts).removeClass("visually-hidden");
-            $(".alerts-dropout").not($alertsDrowout).addClass("visually-hidden");
+            $(".alerts-dropout").not($alertsDropout).addClass("visually-hidden");
             $alerts.toggleClass("visually-hidden");
-            $alertsDrowout.toggleClass("visually-hidden");
+            $alertsDropout.toggleClass("visually-hidden");
         }
     });
 
     // Close dropout menu when clicking outside of it
     $(document).click(function(event) {
-        if (!$(event.target).closest(".profile").length) {
+        if (!$(event.target).closest(".profile").length && !$(event.target).closest(".modal-content").length) {
             $(".profile-options-dropout").addClass("visually-hidden");
             $(".guest-img").removeClass("img-prev-md").addClass("img-prev-sm");
             $(".alerts").removeClass("visually-hidden");
@@ -68,5 +68,12 @@ $(document).ready(function() {
         $("#female-selector").removeClass("selected-selector");
         $("#male-selector").removeClass("selected-selector");
     });
-    
+
+    // Prevent modal from closing when clicking inside the modal content
+    $('.modal-content').on('click', function(event) {
+        if (!$(event.target).closest('[data-dismiss="modal"]').length) {
+            event.stopPropagation();
+        }
+    });
+
 });
